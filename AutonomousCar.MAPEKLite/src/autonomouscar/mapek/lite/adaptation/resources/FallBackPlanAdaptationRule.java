0be17f608de9a025d9ec14b9afca5c0867655d14
miquelLlorca <pkvj.2000@gmail.com>
2026-04-27 15:42:56 +0200

    Requisito empezado

--- AutonomousCar.MAPEKLite/src/autonomouscar/mapek/lite/adaptation/resources/FallBackPlanAdaptationRule.java
+++ AutonomousCar.MAPEKLite/src/autonomouscar/mapek/lite/adaptation/resources/FallBackPlanAdaptationRule.java
@@ -13,26 +13,32 @@ import es.upv.pros.tatami.osgi.utils.interfaces.ITimeStamped;
 import es.upv.pros.tatami.osgi.utils.logger.SmartLogger;
 import sua.autonomouscar.infraestructure.devices.ARC.DistanceSensorARC;
 import sua.autonomouscar.infraestructure.devices.ARC.LineSensorARC;
+import sua.autonomouscar.infraestructure.driving.ARC.FallbackPlanARC;
 import sua.autonomouscar.infraestructure.driving.ARC.L1_DrivingServiceARC;
+import sua.autonomouscar.infraestructure.driving.ARC.L3_DrivingServiceARC;
 import sua.autonomouscar.infraestructure.interaction.ARC.NotificationServiceARC;
 
 
-// SI esta en L3 Y detecta que Road Type es no estandar o off road
-// Entonces degrada a L1
+// SI esta en L3 Y detecta que Road Type es highway o standard
+// Entonces bindea el fallback plan de shoulder, 
+// si no, el default
 
-public class OffRoadAdaptationRule extends AdaptationRule {
+// Requiere acceso al control del motor y la dirección, 
+// 	sensor de distancia lateral derecho, y sensor de carril derecho
+
+public class FallBackPlanAdaptationRule extends AdaptationRule {
 	
-	protected static SmartLogger logger = SmartLogger.getLogger(OffRoadAdaptationRule.class);
-	public static String ID = "Regla ADS-L3-1";
+	protected static SmartLogger logger = SmartLogger.getLogger(FallBackPlanAdaptationRule.class);
+	public static String ID = "Regla ADS-L3-8";
 	
 	IKnowledgeProperty kp_RoadType = null;
 	IKnowledgeProperty kp_nivelConduccion = null;
 	// puede que haga falta añadir las lineas 
 	
-	public OffRoadAdaptationRule(BundleContext context) {
+	public FallBackPlanAdaptationRule(BundleContext context) {
 		super(context, ID);
 		this.setListenToKnowledgePropertyChanges("Tipo_Carretera");
-		//this.setListenToKnowledgePropertyChanges("Nivel_Conducción"); // no haria falta escuchar
+		this.setListenToKnowledgePropertyChanges("Nivel_Conducción"); // no haria falta escuchar
 
 		kp_RoadType = BasicMAPEKLiteLoopHelper.getKnowledgeProperty("Tipo_Carretera");
 		kp_nivelConduccion = BasicMAPEKLiteLoopHelper.getKnowledgeProperty("Nivel_Conduccion");
@@ -42,7 +48,7 @@ public class OffRoadAdaptationRule extends AdaptationRule {
 	@Override
 	public boolean checkAffectedByChange(IKnowledgeProperty property) {
 		
-		if (kp_RoadType == null) {
+		if (kp_RoadType == null || kp_nivelConduccion == null) {
 			logger.trace("Required Knowledge property not set. Not executing the rule ...");
 			return false;
 		}
@@ -54,20 +60,32 @@ public class OffRoadAdaptationRule extends AdaptationRule {
 	
 		
 		String RoadType = null;
+		String Nivel = null;
 		if ( kp_RoadType.getValue() != null ) {
 			RoadType = (String) kp_RoadType.getValue();
 		} else {
 			logger.trace("RoadType NULL! Not executing the rule ...");
 			throw new RuleException("RoadType null value!", "Not executing the rule ...");		
 		}
+		if ( kp_nivelConduccion.getValue() != null ) {
+			Nivel = (String) kp_nivelConduccion.getValue();
+		} else {
+			logger.trace("NivelConduccion NULL! Not executing the rule ...");
+			throw new RuleException("NivelConduccion null value!", "Not executing the rule ...");		
+		}
 		
 	
 
 
-		if ( RoadType.equals("OFF_ROAD")) {
-			return this.configuracionSistemaActivarL1();
+		if (Nivel.equals("L3")) {
+			if(RoadType.equals("HIGHWAY") || RoadType.equals("STD")) {
+				return this.configuracionSistemaActivarShoulder();
+			} else {
+				return this.configuracionSistemaActivarDefault();
+				
+			}
+		}else {
 			
-		} else {
 			logger.trace("Cannot understand knowledge property value. Not executing the rule ...");
 			throw new RuleException("Unknown property value",
 					"Cannot understand knowledge property value. Not executing the rule ...");
@@ -78,43 +96,39 @@ public class OffRoadAdaptationRule extends AdaptationRule {
 	
 	
 	
-	protected IRuleComponentsSystemConfiguration configuracionSistemaActivarL1() {
+	protected IRuleComponentsSystemConfiguration configuracionSistemaActivarShoulder() {
 
 
 		IRuleComponentsSystemConfiguration theNextSystemConfiguration =
 				SystemConfigurationHelper.createPartialSystemConfiguration(
 						this.getId() + "_" + ITimeStamped.getCurrentTimeStamp());
 
-		
 		// Desactivar L3
 		String id = String.format("driving.L3.%s", kp_nivelConduccion);
 		
-		SystemConfigurationHelper.componentToRemove(theNextSystemConfiguration,  id, "1.0.0");
-		// hace falta hacer binding to remove??
+		SystemConfigurationHelper.bindingToAdd(theNextSystemConfiguration, 
+				id, "1.0.0", L3_DrivingServiceARC.REQUIRED_FALLBACKPLAN,
+				"driving.FallbackPlan.ParkInTheRoadShoulder", "1.0.0", FallbackPlanARC.PROVIDED_DRIVINGSERVICE);
+		
+		return theNextSystemConfiguration;		
+		
+	}
 	
-		
-		
-		// Activar L1
-		SystemConfigurationHelper.componentToAdd(theNextSystemConfiguration, "driving.L1.AssistedDriving", "1.0.0");
-		SystemConfigurationHelper.bindingToAdd(theNextSystemConfiguration, 
-				"driving.L1AssistedDriving", "1.0.0", L1_DrivingServiceARC.REQUIRED_FRONTDISTANCESENSOR,
-				"device.FrontDistanceSensor", "1.0.0", DistanceSensorARC.PROVIDED_SENSOR);
-		
-		SystemConfigurationHelper.bindingToAdd(theNextSystemConfiguration, 
-				"driving.L1AssistedDriving", "1.0.0", L1_DrivingServiceARC.REQUIRED_RIGHTLINESENSOR,
-				"device.RightLineSensor", "1.0.0", LineSensorARC.PROVIDED_SENSOR);
-		SystemConfigurationHelper.bindingToAdd(theNextSystemConfiguration, 
-				"driving.L1AssistedDriving", "1.0.0", L1_DrivingServiceARC.REQUIRED_LEFTLINESENSOR,
-				"device.LeftLineSensor", "1.0.0", LineSensorARC.PROVIDED_SENSOR);
-		
-		SystemConfigurationHelper.bindingToAdd(theNextSystemConfiguration, 
-				"driving.L1AssistedDriving", "1.0.0", L1_DrivingServiceARC.REQUIRED_NOTIFICATIONSERVICE,
-				"interaction.NotificationService", "1.0.0", NotificationServiceARC.PROVIDED_SERVICE);
+	
+	protected IRuleComponentsSystemConfiguration configuracionSistemaActivarDefault() {
 
-		SystemConfigurationHelper.setParameter(
-				theNextSystemConfiguration, "driving.L1.AssistedDriving", "1.0.0", L1_DrivingServiceARC.PARAMETER_LONGITUDINALSECURITYDISTANCE,100);
+
+		IRuleComponentsSystemConfiguration theNextSystemConfiguration =
+				SystemConfigurationHelper.createPartialSystemConfiguration(
+						this.getId() + "_" + ITimeStamped.getCurrentTimeStamp());
+
+		// Desactivar L3
+		String id = String.format("driving.L3.%s", kp_nivelConduccion);
 		
-		
+		SystemConfigurationHelper.bindingToAdd(theNextSystemConfiguration, 
+				id, "1.0.0", L3_DrivingServiceARC.REQUIRED_FALLBACKPLAN,
+				"driving.FallbackPlan.Emergency", "1.0.0", FallbackPlanARC.PROVIDED_DRIVINGSERVICE);
+
 		return theNextSystemConfiguration;		
 		
 	}
